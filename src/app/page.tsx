7ea61e419040965d362b6540d8c9214d99e6ca43
niteshsainicoder <kumarsundratijara@gmail.com>
2024-08-11 21:40:00 +0530

    Adding Expretise Section

---
+++ src/app/page.tsx
@@ -0,0 +1,74 @@
+
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "../components/ui/aurora-background";
+import Image from "next/image";
+import { Modal, ModalTrigger } from "@/components/ui/animated-modal";
+export default function Home() {
+
+
+
+  return (
+    <>
+      <AuroraBackground >
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative w-full flex flex-1 gap-3 items-center justify-center px-9"
+        >
+          <div className="relative flex flex-col sm:w-3/6   gap-4 items-center justify-center px-4" >
+            <div className="text-3xl md:text-5xl  w-full bg-clip-text    bg-gradient-to-b from-neutral-200 to-neutral-500 font-bold text-left dark:text-white ">
+              <p className="text-transparent  text-center sm:text-left flex-nowrap antialiased"> Hi, I'm Nitesh Saini</p>
+            </div>
+            <div className="text-base text-center w-full sm:text-left font-semibold antialiased text-neutral-300 sm:text-xl  py-4">
+              A Passionate Web Developer Crafting Solutions for the Future.
+            </div> <div className="absolute -bottom-40 sm:mt-8 sm:static">
+              <Modal  >
+                <ModalTrigger className=" outline outline-neutral-400  antialiased flex justify-center group/modal-btn">
+                  <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500">
+                    Explore  My
+                  </span>
+                  <div className="-translate-x-40  group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500  antialiased z-20">
+                    Jourey
+                  </div>
+                </ModalTrigger>
+              </Modal>
+            </div>
+          </div>
+          <div className="relative hidden max-h-[500px] bg-clip-content sm:h-[500px] min-h-[300px]    sm:flex flex-col  sm:w-3/6 gap-4 items-center justify-center px-4">
+            <Image src={`/download.svg`} alt="Developer" fill priority quality={100} className=" mt-9 bg-blend-screen" />
+          </div>
+
+        </motion.div>
+      </AuroraBackground >
+      <div className="h-fit md:h-screen max-h-screen bg-zinc-900 relative">
+        <div className="w-full text-2xl sm:text-4xl bg-rose-400 font-medium flex justify-center antialiased text-zinc-300 ">
+          <p className=" mt-16">Expertise in</p>
+        </div>
+        <div className="w-full h-full  bg-slate-300 flex justify-center items-start flex-wrap  ">
+
+          <div className="  w-[300px]  h-[300px]">
+            Frontend
+          </div>
+
+
+          <div className=" w-[300px]  bg-lime-300 h-[300px]">
+            Backend
+          </div>
+
+
+          <div className="w-[300px]  bg-amber-400 h-[300px]">
+            DevOps
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,47 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: { aurora: "aurora 60s linear infinite" },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
