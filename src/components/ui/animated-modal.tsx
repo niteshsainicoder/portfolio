ea707eaa8fb349b6a8b686db7f12d7631713b105
niteshsainicoder <kumarsundratijara@gmail.com>
2024-08-24 17:26:41 +0530

    some css changes added

--- src/components/ui/animated-modal.tsx
+++ src/components/ui/animated-modal.tsx
@@ -50,7 +50,7 @@ export const ModalTrigger = ({
     return (
         <button
             className={cn(
-                "px-4 py-2 rounded-lg antialiased text-black dark:text-white text-center relative overflow-hidden",
+                "px-4 py-2 rounded-lg antialiased  text-white text-center relative overflow-hidden",
                 className
             )}
             onClick={() => setOpen(true)}
