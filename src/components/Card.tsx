d118882f3aca8e3f6a668702393c730cac2ee93f
niteshsainicoder <kumarsundratijara@gmail.com>
2024-10-08 12:39:43 +0530

    updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BackgroundGradient } from './ui/background-gradient'
 import Link from 'next/link';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
 interface CardProps {
     title: string; // Define the type for 'title' (adjust as needed)
     repoName:string
@@ -12,19 +13,27 @@ interface CardProps {
 function Card({ title,repoName,extratext="Working on the project",live,image}: CardProps) {
     const [iscliked, setiscliked] = useState(false)
     const [isEnter, setisEnter] = useState(false)
+    const { ref, inView } = useInView({
+        threshold: 0.1, // Adjust as needed
+        triggerOnce: false, // If you want the callback to fire only once
+      });
     const setClicked = () => {
         setiscliked(!iscliked);
   setisEnter(!isEnter)
     }
+
+    useEffect(()=>{
+        
+    },[])
     return (
-        <div onMouseEnter={() => { setisEnter(true) }} onMouseLeave={() => { setisEnter(false) }} className='relative overflow-hidden rounded-3xl '>
+        <div ref={ref} onMouseEnter={() => { setisEnter(true) }} onMouseLeave={() => { setisEnter(false) }} className='relative overflow-hidden rounded-3xl '>
             <BackgroundGradient>
                 <div onClick={() => { setClicked() }} className=" relative overflow-hidden  w-[280px] sm:w-[500px] rounded-[21px] bg-zinc-800 p-4  h-[300px]">
                   <h1>  {title}</h1>
                   <Image src={image} alt="image" fill content='fill'/>
                 </div>
             </BackgroundGradient>
-            <div onClick={() => { setClicked() }} className={`absolute cursor-pointer transform-gpu flex flex-col  justify-end items-left bg-gradient-to-r  from-neutral-700  to-neutral-700 z-10 p-4 opacity-75  h-[300px] w-[280px] sm:w-[500px]  rounded-[21px] top-1  transition ease-in-out duration-700 right-0 bottom-0 ${isEnter || iscliked ? "  -translate-x-1  " : "-translate-x-[550px]   "} `}>
+            <div onClick={() => { setClicked() }} className={`absolute cursor-pointer transform-gpu flex flex-col  justify-end items-left bg-gradient-to-r  from-neutral-700  to-neutral-700 z-10 p-4 opacity-75  h-[300px] w-[280px] sm:w-[500px]  rounded-[21px] top-1  transition ease-in-out duration-700 right-0 bottom-0 ${isEnter || iscliked  ? "  -translate-x-1  " : "-translate-x-[550px]   "} `}>
              <p   className='absolute top-10  p-4 text-xl text-zinc-500 '>{extratext}</p>
               <Link target="_blank" href={`https://github.com/niteshsainicoder/${repoName}`}>  <h5>GitHub Link<span>&#128206;</span> </h5></Link>
               <Link target="_blank" href={`${live}`}>  <h5>Live Link<span>&#128206;</span> </h5></Link>
